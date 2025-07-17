fec8d2b3c10a98543842c7102c5b35abba3eba24
Pedro Carvalho <pedro.godinho.carvalho@gmail.com>
2025-07-17 12:42:18 +0100

    solve problem 7

---
+++ p7.c
@@ -0,0 +1,24 @@
+#include <limits.h>
+
+int reverse(int x){
+    long y = x;
+    
+    // Deal with negative numbers
+    int sign = 1;
+    if (y < 0) {
+        sign = -1;
+        y = -y;
+    }
+
+    // Reverse
+    long result = 0;
+    do {
+        result = result * 10 + (y % 10);
+        y /= 10;
+    } while (y > 0);
+
+    // Protection against overflow
+    if (result > INT_MAX) return 0;
+
+    return result * sign;
+}
